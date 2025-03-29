11481606302c8f581e79b86e4f667282492024fa
rickylin1 <pandanation33@gmail.com>
2025-03-29 16:44:04 -0500

    add socket setup

---
+++ public/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+var id = [];
+
+io.on('connection', (socket) => {
+  id.push(socket.id);
+  console.log(io.engine.clientsCount + ' users connected');
+  socket.on('disconnect', () => {
+    console.log(io.engine.clientsCount + ' users connected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', socket.id + ": " + msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
